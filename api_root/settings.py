e208479707aea97fd28c4321fb635c4736e2887d
anaschlz <analu.sschulz@gmail.com>
2024-05-10 02:17:20 -0300

    update .env

--- api_root/settings.py
+++ api_root/settings.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = 'api_root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'NAME': os.environ.get('POSTGRES_DATABASE'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST':  os.environ.get('POSTGRES_HOST'),
         'PORT': '5432',
     }
